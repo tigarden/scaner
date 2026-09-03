69ff23297023f487cdf575cb3484e2230f7c96ae
Kostyantin Horbunenko <84540383+tigarden@users.noreply.github.com>
2026-09-03 22:06:21 +0300

    feat: initial release of vision ai person or dog detector

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'vision-ai-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
